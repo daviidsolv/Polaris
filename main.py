d1ee394a1d442423ea8144f92bfac31d9a441353
daviidsolv <daviidsolv@gmail.com>
2023-10-12 15:58:09 +0200

    testing main

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+import flet as ft
+
+def on_dialog_result(e: ft.FilePickerResultEvent):
+    print("Selected files:", e.files)
+    print("Selected file or directory:", e.path)
+
+
+
+def main(page: ft.Page):
+    filePicker = ft.FilePicker(on_result=on_dialog_result)
+
+    def upload_files(e):
+        upload_list = []
+        if filePicker.result != None and filePicker.result.files != None:
+            for f in filePicker.result.files:
+                upload_list.append(
+                    ft.FilePickerUploadFile(
+                        f.name,
+                        upload_url=page.get_upload_url(f.name, 600),
+                    )
+                )
+            filePicker.upload(upload_list)
+
+    btn = ft.ElevatedButton("Choose files...",
+    on_click=lambda _: filePicker.pick_files(allow_multiple=True))
+
+    btn2 = ft.ElevatedButton("Upload", on_click=upload_files)
+
+    page.overlay.append(filePicker)
+    page.add(btn)
+    page.add(btn2)
+    page.update()
+
+ft.app(target=main)
